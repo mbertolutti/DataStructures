1f051fbecdfc4bcf74fc0b571fc0542cb7efba47
Mario Bertolutti <mario.bertolutti@gmail.com>
2022-06-22 21:30:50 +0000

    add 'height' command which returns the tree height

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include <stack>
+#include <utility>
 
 struct node
 {
@@ -46,6 +48,37 @@ void random(node*& root, unsigned count)
     }
 }
 
+void height(node*& root)
+{
+    unsigned tree_height = 0;
+    std::stack<std::pair<node*, unsigned>> todo_stack;
+    todo_stack.push(std::make_pair(root, 1));
+    while (!todo_stack.empty())
+    {
+        std::pair<node*, unsigned> current_todo = todo_stack.top();
+        todo_stack.pop();
+        node* current_node = current_todo.first;
+        unsigned current_height = current_todo.second;
+        if (current_node->left)
+        {
+            todo_stack.push(std::make_pair(current_node->left, current_height + 1));
+            if (current_height + 1 > tree_height)
+            {
+                tree_height = current_height;
+            }
+        }
+        if (current_node->right)
+        {
+            todo_stack.push(std::make_pair(current_node->right, current_height + 1));
+            if (current_height + 1 > tree_height)
+            {
+                tree_height = current_height;
+            }
+        }
+    }
+    fmt::print("{}{}{}", "bst height is ", tree_height, "\n\n");
+}
+
 void command_loop(node*& root)
 {
     bool running = true;
@@ -54,6 +87,7 @@ void command_loop(node*& root)
         fmt::print("{}{}", "COMMANDS", "\n");
         fmt::print("{}{}", "\tadd [value]\t inserts value into bst", "\n");
         fmt::print("{}{}", "\trdm [count]\t inserts count random values into bst", "\n");
+        fmt::print("{}{}", "\theight\t\t returns bst height", "\n");
         fmt::print("{}{}", "\tquit\t\t quit program", "\n\n");
         fmt::print("{}", "Command> ");
 
@@ -69,11 +103,15 @@ void command_loop(node*& root)
             insert(root, value);
             fmt::print("{}", "\n");
         }
-        if (input == "rdm")
+        else if (input == "rdm")
         {
             unsigned count;
             std::cin >> count;
             random(root, count);
+        }
+        else if (input == "height")
+        {
+            height(root);
         }
         else if (input == "quit")
         {
